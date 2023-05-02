40c22ba4867d84e6b6c5e45d730bc8083e7bc623
JaeyoonPark <ngs9679@naver.com>
2023-05-03 00:56:38 +0900

    ssl update

--- app/src/main/java/com/example/anyang_setup/LoginActivity.java
+++ app/src/main/java/com/example/anyang_setup/LoginActivity.java
@@ -17,6 +17,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText mUsernameEditText;
@@ -31,9 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
 
         mUsernameEditText = findViewById(R.id.username_edit_text);
-
         mPasswordEditText = findViewById(R.id.password_edit_text);
-
 
         mLoginButton = findViewById(R.id.login_button);
         mAutoLoginCheckBox = findViewById(R.id.my_checkbox);
@@ -41,6 +46,7 @@ public class LoginActivity extends AppCompatActivity {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 String username = mUsernameEditText.getText().toString();
                 String password = mPasswordEditText.getText().toString();
 
@@ -62,15 +68,31 @@ public class LoginActivity extends AppCompatActivity {
 
         @Override
         protected Boolean doInBackground(String... strings) {
+
+
+
             String username = strings[0];
             String password = strings[1];
 
             String loginUrl = "https://tis.anyang.ac.kr/main.do";
+            // SSL 인증서 검증을 무시하도록 설정합니다.
+            TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() { return null; }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) { }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) { }
+            } };
+            try {
+                SSLContext sc = SSLContext.getInstance("SSL");
+                sc.init(null, trustAllCerts, new SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
             try {
                 // Jsoup을 사용하여 로그인 페이지를 가져옵니다.
                 Document loginPage = Jsoup.connect(loginUrl)
-                        .timeout(5000)
+                        .timeout(3000000)
                         .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
                         .referrer("https://tis.anyang.ac.kr/main.do")
                         .method(Connection.Method.GET)
@@ -81,7 +103,7 @@ public class LoginActivity extends AppCompatActivity {
 
                 // 로그인에 필요한 POST 요청을 생성합니다.
                 Connection.Response loginResponse = Jsoup.connect(loginUrl)
-                        .timeout(5000)
+                        .timeout(3000000)
                         .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
                         .referrer("https://tis.anyang.ac.kr/main.do")
                         .data("_csrf", csrfToken)
