4fd512d9b3c5e2b3aba2f41a80c320d2e071cf95
chaesoobum <130902668+chaesoobum@users.noreply.github.com>
2023-05-16 21:56:17 +0900

    05/16

--- app/src/main/java/com/example/anyang_setup/LoginActivity.java
+++ app/src/main/java/com/example/anyang_setup/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.anyang_setup;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
 import android.webkit.WebChromeClient;
@@ -17,15 +19,20 @@ public class LoginActivity extends AppCompatActivity {
     private EditText mUsernameEditText;
     private EditText mPasswordEditText;
     private Button mLoginButton;
+    private Button LoginButton;
     private CheckBox mAutoLoginCheckBox;
     private WebView myWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ex);
+        setContentView(R.layout.login_activity);
 
-        myWebView = findViewById(R.id.web_view);
+        myWebView = findViewById(R.id.webview);
+        mUsernameEditText = findViewById(R.id.username_edit_text);
+        mPasswordEditText = findViewById(R.id.password_edit_text);
+        mLoginButton = findViewById(R.id.login_button);
+        mAutoLoginCheckBox = findViewById(R.id.my_checkbox);
 
         WebViewClient webViewClient = new WebViewClient() {
             @Override
@@ -52,29 +59,66 @@ public class LoginActivity extends AppCompatActivity {
         CookieSyncManager.createInstance(this);
 
         myWebView.loadUrl("https://tis.anyang.ac.kr/main.do#");
+        myWebView.setVisibility(View.GONE);
+
+        // 로그인 버튼 클릭 시 WebView에서 JavaScript 실행
+        mLoginButton.setOnClickListener(v -> {
+            String username = mUsernameEditText.getText().toString();
+            String password = mPasswordEditText.getText().toString();
+
+
+            myWebView.evaluateJavascript(
+                    "javascript:(function() { " +
+                            "    document.getElementById('id').value = '" + username + "'; " +
+                            "    document.getElementById('password').value = '" + password + "'; " +
+                            "    var loginButton = document.querySelector('.login_button'); " +
+                            "    if (loginButton) { " +
+                            "        loginButton.click(); " +
+                            "    } " +
+                            "})();",
+                    null
+            );
+
+            myWebView.setWebViewClient(new WebViewClient() {
+
+
+
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+
+                    // 로그인 버튼의 class 이름이 "login_button"인 경우 클릭 이벤트 처리
+                    if (url.equals("https://tis.anyang.ac.kr/main.do#")) {
+                        myWebView.evaluateJavascript(
+                                "javascript:(function() { " +
+                                        "    var loginButton = document.querySelector('.login_button'); " +
+                                        "    if (loginButton) { " +
+                                        "    loginButton.addEventListener('click', function() { " +
+                                        " window.location.href = 'https://tis.anyang.ac.kr/main.do'; " +
+                                        " }); " +
+                                        " } " +
+                                        "})();",
+                                null
+                        );
+                    }
+                }
+            });
+
+
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+
+
+
+
+
+
+
+        });
 
         // JavaScript로 로그인 버튼 클릭 이벤트 처리
-        myWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-
-                // 로그인 버튼의 class 이름이 "login_button"인 경우 클릭 이벤트 처리
-                if (url.equals("https://tis.anyang.ac.kr/main.do#")) {
-                    myWebView.evaluateJavascript(
-                            "javascript:(function() { " +
-                                    "    var loginButton = document.querySelector('.login_button'); " +
-                                    "    if (loginButton) { " +
-                                    "        loginButton.addEventListener('click', function() { " +
-                                    "            window.location.href = 'https://tis.anyang.ac.kr/main.do'; " +
-                                    "        }); " +
-                                    "    } " +
-                                    "})();",
-                            null
-                    );
-                }
-            }
-        });
     }
 }
-
