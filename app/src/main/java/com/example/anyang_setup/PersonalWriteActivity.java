efe46f1890186e3ad238283981dd95ea98ecddaa
chaesoobum <soobum007@naver.com>
2023-09-19 15:58:40 +0900

    0919

--- app/src/main/java/com/example/anyang_setup/PersonalWriteActivity.java
+++ app/src/main/java/com/example/anyang_setup/PersonalWriteActivity.java
@@ -1,19 +1,18 @@
 package com.example.anyang_setup;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PersonalWriteActivity extends AppCompatActivity {
 
-    Spinner questionSpinner;
-    EditText editText;
+    EditText editText1;
+    EditText editText2;
     Button resetButton;
     Button saveButton;
 
@@ -23,30 +22,30 @@ public class PersonalWriteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_write);
 
-        questionSpinner = findViewById(R.id.personal_question);
-        editText = findViewById(R.id.personal_text);
+        editText1 = findViewById(R.id.personal_question);
+        editText2 = findViewById(R.id.personal_text);
         resetButton = findViewById(R.id.personal_write_reset_button);
         saveButton = findViewById(R.id.personal_write_save_button);
 
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editText.setText("");
-                questionSpinner.setSelection(0);
+                editText1.setText("");
+                editText2.setText("");
             }
         });
 
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String selectedQuestion = questionSpinner.getSelectedItem().toString();
-                String userText = editText.getText().toString();
+                String Question = editText2.getText().toString();
+                String userText = editText1.getText().toString();
 
                 // Intent를 생성하여 PersonalLockerActivity로 전달
                 Intent intent = new Intent(PersonalWriteActivity.this, PersonalLockerActivity.class);
 
                 // 데이터를 intent에 담아 전달
-                intent.putExtra("selectedQuestion", selectedQuestion);
+                intent.putExtra("Question", Question);
                 intent.putExtra("userText", userText);
 
                 startActivity(intent);
